a241df67aab716031f5cfbeb41a193c92389a26d
Bamos Guinley <guinleyb@gmail.com>
2024-08-19 17:34:22 +0200

    up

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args){
+        //exercice 1
         byte aByte=1;
         short aShort;
         int anInt;
@@ -8,43 +9,40 @@ public class App {
         double aDouble;
         char aChar;
         boolean aBoolean;
+        // System.out.println("Taille de int : " + Integer.BYTES + " octets");
+        // System.out.println("Taille de long : " + Long.BYTES + " octets");
+        // System.out.println("Taille de byte : " + Byte.BYTES + " octets");
+        // System.out.println("Taille de float : " + Float.BYTES + " octets");
+        // System.out.println("Taille de char : " + Character.BYTES + " octets");
+        // System.out.println("Taille de double : " + Double.BYTES + " octets");
+        // System.out.println("Taille de short : " + Short.BYTES + " octets");
 
+        //exercice 2
         String str1="Hello ";
         String str2="world";
         String str3=str1+str2;
+        // System.out.println(str3);
+        // System.out.println(str1 == str2);
+        // System.out.println(str1.equals(str2));
 
+        //exercice 3
         int a=2;
         int b=a*2;
         int c=b*2;
+        // System.out.println(a>b && b<a);
 
+        //exercice 4
         int intVar =2;
         double doubleVar=intVar;
         short shortVar = (short) doubleVar;
+        // System.out.println(intVar);
+        // System.out.println(doubleVar);
+        // System.out.println(shortVar);
 
+        //exercice 5
         int x=2;
         float y=2;
         y= (int) y;
-        
-
-    // System.out.println("Taille de int : " + Integer.BYTES + " octets");
-    // System.out.println("Taille de long : " + Long.BYTES + " octets");
-    // System.out.println("Taille de byte : " + Byte.BYTES + " octets");
-    // System.out.println("Taille de float : " + Float.BYTES + " octets");
-    // System.out.println("Taille de char : " + Character.BYTES + " octets");
-    // System.out.println("Taille de double : " + Double.BYTES + " octets");
-    // System.out.println("Taille de short : " + Short.BYTES + " octets");
-
-    // System.out.println(str3);
-    // System.out.println(str1 == str2);
-    // System.out.println(str1.equals(str2));
-    
-    // System.out.println(a>b && b<a);
-
-    // System.out.println(intVar);
-    // System.out.println(doubleVar);
-    // System.out.println(shortVar);
-
-
-    System.out.println(x>y);
+        // System.out.println(x>y);
 }
 }
