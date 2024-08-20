bc31f1f5db61bdf7329f67afeb3fd33b187b33c9
Bamos Guinley <guinleyb@gmail.com>
2024-08-20 18:39:53 +0200

    filter

--- ListArray.java
+++ ListArray.java
@@ -3,6 +3,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ListArray {
+    public static void sortArray(int[] table) {
+        Arrays.sort(table);
+        for (int n : table) {
+            System.out.println(n);
+        }
+    }
+    public static void filtreTable(int[] arg) {
+        for (int i : arg) {
+            if (i % 2 != 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
     public static void main(String[] args) {
         ArrayList<String> nameList = new ArrayList<>();
         // nameList.add("null");
@@ -12,9 +26,7 @@ public class ListArray {
         number.add(4);
         number.add(7);
         int[] table = { 8, 6, 2, 85, 3, 8, 36, 85, 6, 3 };
-        Arrays.sort(table);
-        for (int n :table ) {
-            System.out.println(n);
-        }
+        sortArray(table);
+        filtreTable(table);
     }
 }
